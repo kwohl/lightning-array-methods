b98a8773d71d3d81f810cf0dec7038dca0c0ccb5
Kathryn Wohl <ktwohl@gmail.com>
2020-02-04 13:59:27 -0600

    initial commit

--- main.js
+++ main.js
@@ -147,3 +147,46 @@ const businesses = [
     `
     outEl.innerHTML += "<hr/>"
   });
+
+  //filter method
+
+  // Array to contain all the New York businesses
+const newYorkBusinesses = businesses.filter(business => {
+    let inNewYork = false
+  
+    if (business.addressStateCode === "NY") {
+        inNewYork = true
+    }
+  
+    return inNewYork
+
+  })
+
+  const manEl = document.querySelector("#manufacturing")
+  manEl.innerHTML = "<h1>Manufacturing Businesses</h1>"
+
+  const manufacturingBusinesses = businesses.filter(business => {
+      let manufacturing = false
+      
+      if (business.companyIndustry === "manufacturing") {
+          manufacturing = true
+      }
+
+    return manufacturing
+  })
+
+  console.log(manufacturingBusinesses)
+
+  manufacturingBusinesses.forEach(business => {
+    manEl.innerHTML += `
+    <h2>${business.companyName}</h2>
+    <section>
+        ${business.addressFullStreet}
+    </section>
+    <section>
+        ${business.addressCity}, ${business['addressStateCode']} ${business['addressZipCode']}
+    </section>
+    `
+    manEl.innerHTML += "<hr/>"
+})
+  
