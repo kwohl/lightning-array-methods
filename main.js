1da17129735dae22427d7420cdf53f6566772d27
Kathryn Wohl <ktwohl@gmail.com>
2020-02-04 18:45:55 -0600

    initial map practice

--- main.js
+++ main.js
@@ -193,3 +193,31 @@ const newYorkBusinesses = businesses.filter(business => {
 
 // map method
   
+outEl.innerHTML += "<h1>Purchasing Agents</h1>";
+
+/*
+    Using map(), you extract the purchasing agent object
+    from each business and store it in a new array
+*/
+const agents = businesses.map(business => {
+    return {business.purchasingAgent,
+    business.companyName,
+    business.phoneWork}
+})
+
+const companies = businesses.map(business => {
+    return business.companyName
+})
+
+const phone = businesses.map(business => {
+    return business.phoneWork
+})
+
+console.table(agents, companies, phone)
+
+agents.forEach(agent => {
+  outEl.innerHTML += `<h2>${agent.nameFirst} ${agent.nameLast}</h2>`
+                        outEl.innerHTML += "<hr/>";
+});
+
+const agentsInfo = businesses.map(function callback( ))
